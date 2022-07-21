258a1e6d51feb21ba2e560e69c0563a3aa6b62cb
chinosalva <ericksalvadorhernandez44@gmail.com>
2022-07-21 15:05:03 -0500

    Carpeta Tareas

---
+++ 1-intro/algoritmosIntro/app.js
@@ -0,0 +1,20 @@
+//alert("Hello world!")
+//variables
+
+let miVariable = "Chino"
+
+miVariable = "Pedro"
+
+console.log(miVariable)
+
+const apellido = "Sanchez"
+console.log(apellido)
+
+let num1 = "456"
+console.log(typeof num1 )
+
+let nombre = "Salvador"
+let numeroFavorito = 8
+let mascotas = "Si"
+
+console.log(`${nombre} ${numeroFavorito} ${mascotas}`)
---
+++ ALGOITMOSSOLUCIONES/assets/js/app.js
@@ -0,0 +1,62 @@
+// let edad = parseInt (prompt("cual es tu edad"))
+
+// if (edad >= 18) {
+//     alert("Eres mayor de edad")
+// }else{
+//     alert("Eres menor de edad")
+// 
+/* let nombre = prompt("Cual es tu nombre")
+let sueldoD = 600
+let semana = 5
+let mes = 4
+let sueldoSemal = sueldoD * semana
+let sueldoMensual = mes * sueldoSemal
+console.log(`Hola ${nombre}`)
+console.log(`Tu sueldo diario es de ${sueldoD} `)
+console.log(`Tu sueldo semanal ${sueldoSemal}`)
+console.log(`Tu sueldo mensual ${sueldoMensual}`)
+ */
+/* 
+    let num = prompt("Dime un numero")
+
+        
+            function divisible (num){
+                if (num % 7 === 0 && num % 8 === 0){
+                    //return true
+                    console.log(true)
+                }else{
+                    console.log(false)
+                    //return false
+                }
+            }
+    
+        divisible()
+ */
+    
+        //crea un archivo llamado ejercicio1.js
+        //cada comando debe mostrarse en consola
+        
+        const personas = ["Maria", "Dani", "Luis", "Juan", "Camila"];
+        
+        //Escribe un comando para remover a "Dani" del arreglo
+        personas.splice()
+
+
+
+        //Escribe un comando para remover a "Juan del arreglo"
+        
+        //Escribe un comando para agregar a "Luis" al inicio del arreglo
+        
+        //Escribe el comando para agregar tu nombre al final del arreglo
+        
+        //Escribe el comando para mostrar la posición de "Maria"
+        
+        //Escribe el comando para mostrar la posición de tu nombre
+        
+
+
+
+
+
+
+
