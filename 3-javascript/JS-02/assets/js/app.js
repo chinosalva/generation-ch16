76ad0eec9fc137625eb4395cd050199895d82353
chinosalva <ericksalvadorhernandez44@gmail.com>
2022-08-02 17:01:55 -0500

    Baby Steps Funtions

---
+++ 3-javascript/JS-02/assets/js/app.js
@@ -0,0 +1,10 @@
+function miSuma (a, b){
+    console.log(`tu resultado es ${a + b}`)
+    return a + b 
+}
+miSuma(8, 9)
+
+function miSuma1 (a, b){
+    return (a + b)
+}
+console.log(`El producto es ${miprducto(4, 5)}`)
