2466317d5d56fb6f3372cc22e8dd714d45f79e38
chinosalva <ericksalvadorhernandez44@gmail.com>
2022-08-02 17:25:55 -0500

    Ejercicos funciones terminado

--- 3-javascript/JS-02/assets/js/app.js
+++ 3-javascript/JS-02/assets/js/app.js
@@ -8,3 +8,22 @@ function miSuma1 (a, b){
     return (a + b)
 }
 console.log(`El producto es ${miprducto(4, 5)}`)
+let res
+function miResta (a, b){
+    return (a - b)
+}
+res = miResta(10, 4)
+console.log(`La resta es ${res}`)
+
+function miName (nombre, paterno, materno){
+    console.log(`Mi nombre es ${nombre} ${paterno} ${materno}`)
+}
+miName(Erick, Hernandez, Monteagudo)
+
+function division (a, b){
+    console.log(`El resultado de dividir ${a} entre ${b} es ${a / b}`)
+}
+
+function cociente (a, b){
+    console.log(`El cociente de ${a} entre ${b} es ${a % b}`)
+}
