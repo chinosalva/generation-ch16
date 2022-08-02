a0d0f242e528d45ff88a03989d0eb48e692ef0d4
chinosalva <ericksalvadorhernandez44@gmail.com>
2022-08-02 10:41:32 -0500

    Primeros pasos JavaScript

---
+++ 3-javascript/JS-01/assets/js/app.js
@@ -0,0 +1,21 @@
+// Tipos de datos
+// Tipos de variables
+let a = 20
+const b = 0
+var c 
+const PI = 3.1416
+
+// String
+let cadena = 'Hola Mundo!'
+// Number
+let numero = 5
+let numero2 = 5.89
+let numero3 = -5.89
+// Scope - contexto
+// espacio en el que vive la variable
+if (true) {
+    let ambito = "Esta es mi casa"
+}else {
+    ambito = "El patio"
+}
+let fuera = "Aqui ya no es mi casa"
