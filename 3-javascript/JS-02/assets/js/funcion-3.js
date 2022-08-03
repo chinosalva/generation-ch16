585e17932202ec3b7685b88ce181de064ea2f85e
chinosalva <ericksalvadorhernandez44@gmail.com>
2022-08-03 12:18:43 -0500

    Funciones Nuevos Conocimientos

---
+++ 3-javascript/JS-02/assets/js/funcion-3.js
@@ -0,0 +1,18 @@
+/* Funciones Como Objetos  */ 
+function miFuncion (a, b){
+    let res = 0
+    res = (a + b)
+    return res
+}
+
+console.log(typeof miFuncion)
+var miFuncionTexto = miFuncion.toString()
+console.log(miFuncionTexto)
+
+
+
+
+
+
+
+
