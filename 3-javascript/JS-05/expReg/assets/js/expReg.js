515a12bf50d0f141c4cd2187b55edebc12ba0cff
chinosalva <ericksalvadorhernandez44@gmail.com>
2022-08-08 10:45:57 -0500

    final de expreciones regulares con victor

--- 3-javascript/JS-05/expReg/assets/js/expReg.js
+++ 3-javascript/JS-05/expReg/assets/js/expReg.js
@@ -6,7 +6,10 @@
 // encontrar = encontrar.test(regular)
 // console.log(encontrar)
 
-let buscar = 'te marque anoche'
-let buscar1 = /[eo]/
-buscar1 = buscar1.test(buscar)
-console.log(buscar1)
+// let buscar = 'te marque anoche'
+// let buscar1 = /[eo]/
+// buscar1 = buscar1.test(buscar)
+// console.log(buscar1)
+// let texto1 = "los numeros primos son 2 3 5 7 dentro de los 10 primeros numeros"
+
+// let buscar =/\d{3}/
