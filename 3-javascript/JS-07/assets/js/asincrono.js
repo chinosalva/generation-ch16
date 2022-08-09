81e160f9962f942c3857896f042532fe6e43c221
chinosalva <ericksalvadorhernandez44@gmail.com>
2022-08-09 10:40:38 -0500

    Asincrono y sincrono

---
+++ 3-javascript/JS-07/assets/js/asincrono.js
@@ -0,0 +1,14 @@
+console.log(1)
+
+console.log(2)
+
+console.log(3)
+
+setTimeout(() => {
+console.log("Hola")
+}, 2000)
+
+
+console.log(4)
+
+console.log(5)
---
+++ 3-javascript/JS-07/assets/js/promesas.js
@@ -0,0 +1,15 @@
+const saludo = "Hola Mundo"
+const datos = [
+    {
+        nombre: "Jonatan",
+        apellido: "Vazques"
+    },
+    {
+        nombre: "Jonatan",
+        apellido: "Vazques"
+    },
+    {
+        nombre: "Jonatan",
+        apellido: "Vazques"
+    }
+]
